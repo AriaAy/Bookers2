226e7d2a5c9698f26a1d9808ecc1127d1c4d07d4
AriaAy <128759543+AriaAy@users.noreply.github.com>
2023-05-08 21:57:33 +0000

    [update] 4-25 フレームワークの調整

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books
   resources :users, only: [:create, :index, :show, :edit, :update]
   
   get 'homes/about' => 'homes#about'
